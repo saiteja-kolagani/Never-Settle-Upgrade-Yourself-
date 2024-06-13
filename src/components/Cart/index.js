2c96f982a83780528146558a9422dfb901e85f04
SaiTejaKolagani <saiteja.kolagani@gmail.com>
2024-06-13 10:58:09 +0000

    Updated

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,3 +1,5 @@
+import {ImSpinner9} from 'react-icons/im'
+
 import Header from '../Header'
 import './index.css'
 
@@ -10,6 +12,10 @@ const Cart = () => (
         alt="cart"
         className="cart-img"
       />
+      <div className="cart-progress-container">
+        <ImSpinner9 className="cart-spinner animate-spin" />
+        <h1 className="cart-progress-text">DEVELOPMENT IN PROGRESS</h1>
+      </div>
     </div>
   </>
 )
--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -11,17 +11,21 @@ const SimilarProductItem = props => {
         className="similar-product-image"
         alt={`similar product ${title}`}
       />
-      <p className="similar-product-title">{title}</p>
-      <p className="similar-products-brand">by {brand}</p>
-      <div className="similar-product-price-rating-container">
-        <p className="similar-product-price">Rs {price}/-</p>
-        <div className="similar-product-rating-container">
-          <p className="similar-product-rating">{rating}</p>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
-            alt="star"
-            className="similar-product-star"
-          />
+      <div className="similar-product-item-content-container">
+        <div className="sub-similar-product-content-container">
+          <p className="similar-product-title">{title}</p>
+          <p className="similar-products-brand">by {brand}</p>
+          <div className="similar-product-price-rating-container">
+            <p className="similar-product-price">Rs {price}/-</p>
+            <div className="similar-product-rating-container">
+              <p className="similar-product-rating">{rating}</p>
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+                alt="star"
+                className="similar-product-star"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </li>
